2ae7e03db49b8ed0e7355356cdf68dc463e9620d
hany karam <hanykaram63@gmail.com>
2021-05-15 23:02:46 +0200

    iOS

--- History/ViewController.swift
+++ History/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FSCalendarDelegate {
+    @IBOutlet weak var calender: FSCalendar!
+    var history:String?
+    var calendar = FSCalendar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calender.delegate = self
+    }
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let foramtter = DateFormatter()
+        foramtter.dateFormat = "YYYY-MM-dd"
+        let string = foramtter.string(from:date)
+        print("\(string)")
+        history = string
+        print("history",history ?? "")
+
     }
 
 
